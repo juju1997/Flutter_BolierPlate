6dba4fb48afac1d724fac167396b18dbe8beeb7e
HyeongJin Ju <gudwls671214@gmail.com>
2022-02-12 22:16:35 +0900

    색 재정의

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -10,13 +10,13 @@ class AppColors {
 
   // common
   static const Color backGround = Color(0xFFFFF9EC);
-  static const Color spinKit = Color(0xFFF9BE7C);
+  static const Color spinKit = Color(0xFFFF9200); //0xFFF9BE7C
 
   // auth
-  static const Color authBtnActive = Color(0xFFF9BE7C);
+  static const Color authBtnActive = Color(0xFFFF9200); //0xFFF9BE7C
   static const Color authBtnInActive = Color(0xFFFFE4C7);
-  static const Color authIcon = Color(0xFFF9BE7C);
-  static const Color emailText = Color(0xFFE39666);
+  static const Color authIcon = Color(0xFFFF9200); //0xFFF9BE7C
+  static const Color emailText = Color(0xFFFF9200);
 
 
 
